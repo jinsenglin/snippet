8ba715b3bc392cb04bfb0ea620465d18c3101d05
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2016-07-25 14:18:07 +0800

    update python/setup.py

--- python/setup.py
+++ python/setup.py
@@ -21,7 +21,6 @@ setup(name='pysperm',
       license="Apache 2.0",
       classifiers=[
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.5',
       ],
       install_requires=[]
       )
